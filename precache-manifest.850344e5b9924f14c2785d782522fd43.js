a07fddc887231c4bd056bd268ac55f5a25d7eb61
manish-dalal <md4640@gmail.com>
2020-03-01 22:26:53 +0530

    Updates

---
+++ precache-manifest.850344e5b9924f14c2785d782522fd43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d6b422d44db5359a31da9cd02d5870",
+    "url": "/redux_curd_operation/index.html"
+  },
+  {
+    "revision": "abe3684ca4c3b8ae16a8",
+    "url": "/redux_curd_operation/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ed65021645aacf246abb",
+    "url": "/redux_curd_operation/static/css/main.5d22ab48.chunk.css"
+  },
+  {
+    "revision": "abe3684ca4c3b8ae16a8",
+    "url": "/redux_curd_operation/static/js/2.38a7a41e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/redux_curd_operation/static/js/2.38a7a41e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed65021645aacf246abb",
+    "url": "/redux_curd_operation/static/js/main.af287f69.chunk.js"
+  },
+  {
+    "revision": "2f7b717f1010111b06ea",
+    "url": "/redux_curd_operation/static/js/runtime-main.c8b9ca29.js"
+  }
+]);
